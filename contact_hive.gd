f23457f36c460b98767e8eb1606cfd01f00bc4e9
Zin <chasehap@gmail.com>
2024-02-08 15:37:52 -0500

    Began development of HTTP server search.

--- contact_hive.gd
+++ contact_hive.gd
@@ -1,5 +1,6 @@
 extends Button
 
+var address = "localhost:3621"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,5 +10,14 @@ func _ready():
 func _process(delta):
 	pass
 
+func _on_line_edit_text_changed(new_text):
+	address = new_text
+
 func _on_pressed():
-	pass
+	print(address)
+	#$HTTPRequest.request_completed.connect(_on_request_completed)
+	$HTTPRequest.request("https://api.github.com/repos/godotengine/godot/releases/latest")
+
+func _on_request_completed(result, response_code, headers, body):
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print(json["url"])
