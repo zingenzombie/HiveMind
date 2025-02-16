2332f5830820383cfd27a68affc207f2adcc120a
Chase Hap <chasehap@gmail.com>
2025-02-15 21:15:39 -0500

    Implementing smoothing.

--- HiveMindUnityClient/Assets/OtherClient.cs
+++ HiveMindUnityClient/Assets/OtherClient.cs
@@ -4,4 +4,25 @@ public class OtherClient : MonoBehaviour
 {
     public string playerID;
     public string username = "Unnamed";
+
+    Transform oldTramsform, newTransform, targetTransform;
+    private Vector3 velocity = Vector3.zero;
+
+    float timeDif = Time.time;
+
+    void FixedUpdate()
+    {
+        oldTramsform = transform;
+        targetTransform = newTransform;
+        timeDif = Time.time;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(oldTramsform.position, targetTransform.position, ref velocity, (Time.time - timeDif) / Time.fixedDeltaTime);
+    }
+
+    public void ChangeTarget(Transform newTransform){
+        this.newTransform = newTransform;
+    }
 }
